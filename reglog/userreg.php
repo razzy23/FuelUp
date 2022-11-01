3781bda23215ac75331794655e4e835f05f8903c
razzy23 <raviisas666@outlook.com>
2022-11-01 13:14:49 +0530

    logins updated

--- reglog/userreg.php
+++ reglog/userreg.php
@@ -6,62 +6,33 @@
     <h1>Data recieved from the user registration form</h1>
     <p>
       <?php
+      session_start();
 
-        $db_host = 'localhost';
-        $db_user = 'root';
-        $db_password = 'root';
-        $db_db = 'FuelUp';
+      $_SESSION['nic'] = $_POST['nic'];
+      $phone=$_POST['phone'];
+      $_SESSION['phone'] = $phone;
+      $_SESSION['uname'] = $_POST['uname'];
+      $email = $_POST['email'];
+      $_SESSION['email'] = $email;
+      $pass = $_POST['pass'];
+      $_SESSION ['pass'] = $pass;
+      $conf = $_POST['conf']; 
+      //getting data from the form
 
-        $mysqli = @new mysqli(
-          $db_host,
-          $db_user,
-          $db_password,
-          $db_db
-        );
+      if($pass != $conf){
+        die ("Registration error:<br/>Passwords do not match");
+      }
+      if(!filter_var($email, FILTER_VALIDATE_EMAIL) && !$email==null){
+        die ("Registration error:<br/>Invalid email");
+      }
+      if(strlen($nic) > 12){
+        die ("Invalid NIC");
+      }
+      //validation
 
-        if ($mysqli->connect_error) {
-          die('Connection error error:<br/>Errno: '.$mysqli->connect_errno.'<br/>Error'.$mysqli->connect_error);
-        }
-        echo('<br/>Success: A proper connection to MySQL was made.<br/> Host information: '.$mysqli->host_info.'<br/> Protocol version: '.$mysqli->protocol_version);
-        //connection to mySQL database
+      header("Location: vehreg.html", true, 301);
 
-
-        $nic = $_POST['nic'];
-        $phone = $_POST['phone'];
-        $uname = $_POST['uname'];
-        $email = $_POST['email'];
-        $pass = $_POST['pass'];
-        $conf = $_POST['conf'];
-        $terms = filter_input(INPUT_POST, 'terms', FILTER_VALIDATE_BOOLEAN); 
-        //getting data from the form
-
-        if($pass != $conf){
-          die ("Registration error:<br/>Passwords do not match");
-        }
-        if(!filter_var($email, FILTER_VALIDATE_EMAIL) && !$email==null){
-          die ("Registration error:<br/>Invalid email");
-        }
-        if(!$terms){
-          die ("Registration error:<br/>You must agree to the terms and conditions");
-        }
-        if(strlen($nic) > 12){
-          die ("Invalid NIC");
-        }
-        //validation
-
-        $sql = "INSERT INTO users (nic, phone, name, email, password) VALUES ('$nic', '$phone', '$uname', '$email', '$pass')";
-        //sql query to insert data to the account if valid
-
-        if ($mysqli->query($sql) === TRUE) {
-          echo("<br/>Account created successfully");
-          header("Location: ulogin.html", true, 301);
-          //redirect to login page
-        } else {
-          echo ("<br/>Error: " . $sql . "<br>" . $mysqli->error);
-          //insert error
-        }
-        $mysqli->close();
-      ?>
+?>
     </p>
   </body>
 </html>
---
+++ reglog/vehreg.php
@@ -0,0 +1,62 @@
+<?php
+
+    session_start();
+    $db_host = 'localhost';
+    $db_user = 'root';
+    $db_password = 'root';
+    $db_db = 'FuelUp';
+
+    $mysqli = @new mysqli(
+        $db_host,
+        $db_user,
+        $db_password,
+        $db_db
+    );
+
+    if ($mysqli->connect_error) {
+        die('Connection error error:<br/>Errno: '.$mysqli->connect_errno.'<br/>Error'.$mysqli->connect_error);
+    }
+    echo('<br/>Success: A proper connection to MySQL was made.<br/> Host information: '.$mysqli->host_info.'<br/> Protocol version: '.$mysqli->protocol_version);
+    //connection to mySQL database
+
+
+    $nic = $_SESSION['nic'];
+    $phone = $_SESSION['phone'];
+    $uname = $_SESSION['uname'];
+    $email = $_SESSION['email'];
+    $pass = $_SESSION['pass'];
+    $vnum = explode("-", $_POST['vnum'],2);
+    $let = $vnum[0];
+    $num = $vnum[1];
+    $chas = $_POST['chassis'];
+    $vtype = $_POST['type'];
+    $fuel = $_POST['fuel'];
+    $terms = filter_input(INPUT_POST, 'terms', FILTER_VALIDATE_BOOLEAN); 
+    //getting data from the form
+
+    
+    if(!$terms){
+        die ("Registration error:<br/>You must agree to the terms and conditions");
+    }
+    //validation
+
+    $sql1 = "INSERT INTO users (nic, phone, name, email, password) VALUES ('$nic', '$phone', '$uname', '$email', '$pass')";
+    $sql2 ="INSERT INTO vehicles VALUES ('$nic', '$let', '$num', '$vtype', '$fuel', '$chas')";
+
+    if ($mysqli->query($sql1) === TRUE) {
+        echo("<br/>Account created successfully");
+        if($mysqli->query($sql2) === TRUE){
+            echo("<br/>Vehicle registered successfully");
+            header("Location: dash.html", true, 301);
+        }
+        else{
+            echo("<br/>Error: ".$sql2."<br/>".$mysqli->error);
+        }
+        
+        //redirect to login page
+    } else {
+        echo ("<br/>Error: " . $sql . "<br>" . $mysqli->error);
+        //insert error
+    }
+    $mysqli->close();
+?>
